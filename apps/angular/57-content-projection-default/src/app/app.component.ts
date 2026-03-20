4601727226c054e9267a126f81cb8aa9475ed9a8
Azucena <azu.santos.ete@gmail.com>
2026-03-20 10:37:51 +0100

    feat: usar el ng-content con valores por defecto y usar varios

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CardComponent } from './card.component';
 
 @Component({
   imports: [CardComponent],
   selector: 'app-root',
+  schemas: [NO_ERRORS_SCHEMA],
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <!-- <app-card title="Titre 1" message="Message1" />
+    <app-card title="Titre 2" /> -->
+    <app-card>
+      <card-title>Title 1</card-title>
+      <card-message>Message 1</card-message>
+    </app-card>
+    <app-card>
+      <card-title>Title 2</card-title>
+    </app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
--- apps/angular/57-content-projection-default/src/app/card.component.ts
+++ apps/angular/57-content-projection-default/src/app/card.component.ts
@@ -1,15 +1,19 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   imports: [],
   template: `
-    <div>{{ title() }}</div>
+    <!-- <div>{{ title() }}</div>
     @if (message()) {
       <div>{{ message() }}</div>
     } @else {
       <div>Aucun message</div>
-    }
+    } -->
+    <ng-content select="card-title"></ng-content>
+    <ng-content select="card-message">Aucum mesagge</ng-content>
+    <!--si no tenemos un card-message dentro del card.componente llamado,
+    se va a mostrar el contenido que tengamos dentro del ng content (como si fuese un valor por defecto)-->
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
@@ -17,6 +21,6 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   },
 })
 export class CardComponent {
-  title = input.required<string>();
-  message = input<string | undefined>(undefined);
+  // title = input.required<string>();
+  // message = input<string | undefined>(undefined);
 }
